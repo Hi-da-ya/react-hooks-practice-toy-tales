020f318eed552848c97b2a577aa53ebf87847512
Hidaya Wetsakhulia <hidayawetsa@gmail.com>
2024-05-02 14:09:48 +0300

    initial commit

--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,15 +1,38 @@
+import axios from "axios";
 import React from "react";
+import { useState } from "react";
 
-function ToyForm() {
+function ToyForm({addToy}) {
+  const [formData, setFormData] = useState({
+    name:"",
+    image:"",
+  })
+
+  function handleChange(event){
+    setFormData({
+      ...formData, [event.target.name]:event.target.value,
+    })
+  }
+
+  function handleSubmit(event){
+    event.preventDefault();
+    const newToy = {
+      ...formData, likes:0
+    }
+    axios.post("http://localhost:3001/toys", newToy)
+    .then((res) => addToy(res.data))
+  }
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          onChange={handleChange}
+          value={formData.name}
         />
         <br />
         <input
@@ -17,6 +40,8 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          onChange={handleChange}
+          value={formData.image}
         />
         <br />
         <input
